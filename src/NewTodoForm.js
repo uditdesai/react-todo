df8fd631cbfd23c98994edf0aed398f26d7b5bb3
Udit Desai <uydesai@Udits-MacBook-Pro.local>
2019-06-05 11:08:03 -0400

    new todo and edit todo functionality

---
+++ src/NewTodoForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import uuid from 'uuid/v4';
+
+class NewTodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { task: "" };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        this.props.createTodo({ ...this.state, id: uuid() });
+        this.setState({
+            task: ""
+        });
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="task">New Todo</label>
+                <input
+                    id="task"
+                    name="task"
+                    type="text"
+                    placeholder="new task"
+                    value={this.state.task}
+                    onChange={this.handleChange}
+                />
+                <button>Add Todo</button>
+            </form>
+        )
+    }
+}
+
+export default NewTodoForm;
---
+++ src/TodoList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { todos: [] };
+        this.create = this.create.bind(this);
+        this.remove = this.remove.bind(this);
+    }
+    create(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+    }
+    remove(id) {
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        });
+    }
+    render() {
+        const todos = this.state.todos.map(todo => {
+            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />
+        })
+        return (
+            <div>
+                <h1>Todo List</h1>
+                <NewTodoForm createTodo={this.create} />
+                <ul>
+                    {todos}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
